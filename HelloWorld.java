39fae5990c3d031834bdd8976f43009a256eb0db
RaffBalayan <Rafikbalayan@gmail.com>
2021-10-25 15:50:24 +0400

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
     public static void main(String args[]){
-     System.out.println("Hello FROM The Java");
+     System.out.println("Hello from  The Java");
+     mI
     }
 }
 
